2fbb04450ebb020e34e3bc4b9175f0160e488f5a
Alexander Bugge Stage <abs@stubbe.dev>
2023-12-23 12:19:20 +0100

    added commenting

---
+++ src/nvim/lua/plugins/utils.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {
+        disable_filetype = { "TelescopePrompt" , "vim" },
+    }
+  },
+  {
+    'echasnovski/mini.comment',
+    version = '*',
+    opts = {
+      options = {
+        ignore_blank_line = true,
+        start_of_line = false,
+        pad_comment_parts = true,
+      },
+    }
+  }
+}
+
