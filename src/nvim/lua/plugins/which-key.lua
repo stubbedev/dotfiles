a0fceae5134c15e80a870f985b3d805f139a1a45
Alexander Bugge Stage <abs@stubbe.dev>
2023-12-23 14:08:47 +0100

    toggle mark

--- src/nvim/lua/plugins/which-key.lua
+++ src/nvim/lua/plugins/which-key.lua
@@ -10,6 +10,32 @@ return {
   },
   {
     "chentoast/marks.nvim",
-    opts = {}
+    opts = {
+      default_mappings = false,
+      cyclic = true,
+      builtin_marks = { ".", "<", ">", "^", "'" },
+      mappings = {
+        toggle = '<leader>mn',
+        annotate = '<leader>ma',
+        next = '<leader>n',
+        prev = '<leader>N',
+        preview = '<leader>mp',
+        delete_line = '<leader>md',
+        delete_buf = '<leader>me',
+        set_bookmark0 = '<leader>m0',
+        set_bookmark1 = '<leader>m1',
+        set_bookmark2 = '<leader>m2',
+        set_bookmark3 = '<leader>m3',
+        set_bookmark4 = '<leader>m4',
+        set_bookmark5 = '<leader>m5',
+        set_bookmark6 = '<leader>m6',
+        set_bookmark7 = '<leader>m7',
+        set_bookmark8 = '<leader>m8',
+        set_bookmark9 = '<leader>m9',
+        next_bookmark = '<leader>mb',
+        prev_bookmark = '<leader>mB',
+        delete_bookmark = '<leader>mD',
+      }
+    },
   }
 }
