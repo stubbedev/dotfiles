9a7913e50aa64e27aa962e42d0e592f2c036da78
Alexander Bugge Stage <abs@stubbe.dev>
2023-12-25 11:34:39 +0100

    added hardmode

--- src/nvim/lua/plugins/utils.lua
+++ src/nvim/lua/plugins/utils.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      max_count = 3,
+      disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+    },
+    keys = {
+      { '<leader>ht', '<cmd>Hardtime toggle<cr>', desc = "Toggle HardTime" },
+    }
+  },
+  {
     "aserowy/tmux.nvim",
     opts = {}
   },
