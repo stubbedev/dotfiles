0b76c12a8484d927c6f1b4ef3e90780cbca449e8
Alexander Bugge Stage <abs@stubbe.dev>
2023-12-29 23:42:02 +0100

    none ls

--- src/nvim/lua/plugins/none-ls.lua
+++ src/nvim/lua/plugins/none-ls.lua
@@ -1,44 +1,78 @@
 return {
-  "nvimtools/none-ls.nvim",
-  lazy = false,
-  config = function()
-    local null_ls = require("null-ls")
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		lazy = false,
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "nvimtools/none-ls.nvim" },
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			require("mason-null-ls").setup({
+				automatic_installation = true,
+				ensure_installed = {
+					"stylua",
+					"gitsigns",
+					"spell",
+					"luasnip",
+					"tags",
+					"gospel",
+					"eslint",
+					"php",
+					"phpcs",
+					"phpstan",
+					"phpcbf",
+					"phpcsfixer",
+					"pylint",
+					"ruff",
+					"typos",
+					"zsh",
+					"alex",
+					"dictionary",
+					"autoflake",
+					"goimports",
+					"prettier",
+					"rustfmt",
+				},
+			})
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.shellcheck,
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.completion.luasnip,
-        null_ls.builtins.completion.tags,
-        null_ls.builtins.diagnostics.gospel,
-        null_ls.builtins.diagnostics.luacheck,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.php,
-        null_ls.builtins.diagnostics.phpcs,
-        null_ls.builtins.diagnostics.phpstan,
-        null_ls.builtins.diagnostics.pylint.with({
-          diagnostics_postprocess = function(diagnostic)
-            diagnostic.code = diagnostic.message_id
-          end,
-        }),
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.diagnostics.typos,
-        null_ls.builtins.diagnostics.zsh,
-        null_ls.builtins.hover.dictionary,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.autoflake,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.phpcbf,
-        null_ls.builtins.formatting.phpcsfixer,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.diagnostics.alex,
-      },
-    })
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.code_actions.gitsigns,
+					null_ls.builtins.code_actions.shellcheck,
+					null_ls.builtins.completion.spell,
+					null_ls.builtins.completion.luasnip,
+					null_ls.builtins.completion.tags,
+					null_ls.builtins.diagnostics.gospel,
+					null_ls.builtins.diagnostics.luacheck,
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.diagnostics.php,
+					null_ls.builtins.diagnostics.phpcs,
+					null_ls.builtins.diagnostics.phpstan,
+					null_ls.builtins.diagnostics.pylint.with({
+						diagnostics_postprocess = function(diagnostic)
+							diagnostic.code = diagnostic.message_id
+						end,
+					}),
+					null_ls.builtins.diagnostics.ruff,
+					null_ls.builtins.diagnostics.typos,
+					null_ls.builtins.diagnostics.zsh,
+					null_ls.builtins.diagnostics.alex,
+					null_ls.builtins.hover.dictionary,
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.autoflake,
+					null_ls.builtins.formatting.goimports,
+					null_ls.builtins.formatting.phpcbf,
+					null_ls.builtins.formatting.phpcsfixer,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.rustfmt,
+				},
+			})
 
-    vim.keymap.set("n", "gf", function()
-      vim.lsp.buf.format()
-    end, { desc = "Format buffer null_ls" })
-  end,
+			vim.keymap.set("n", "gf", function()
+				vim.lsp.buf.format()
+			end, { desc = "Format buffer null_ls" })
+		end,
+	},
 }
