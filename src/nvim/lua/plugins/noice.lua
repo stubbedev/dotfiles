dd517ce677290c3906163cade96293a1ee164ddd
Alexander Stage <alexander.bugge.stage@gmail.com>
2023-12-30 09:55:56 +0100

    formatting

--- src/nvim/lua/plugins/noice.lua
+++ src/nvim/lua/plugins/noice.lua
@@ -17,7 +17,7 @@ return {
         command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
+        lsp_doc_border = true,       -- add a border to hover docs and signature help
       },                              -- add any options here
       routes = {
         {
@@ -32,7 +32,7 @@ return {
             col = "50%",
           },
           size = {
-            width = 60,
+            width = 80,
             height = "auto",
           },
         },
@@ -43,8 +43,8 @@ return {
             col = "50%",
           },
           size = {
-            width = 60,
-            height = 10,
+            width = 80,
+            height = "auto",
           },
           border = {
             style = "none",
