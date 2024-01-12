b15250e2cfa64097e70d4db819674e1fa0d45acc
Alexander Stage <abs@stubbe.dev>
2024-01-12 20:54:23 +0100

    refactor lazyvim

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -1,20 +1,2 @@
--- Stubbe NEOVIM Config
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("vim-options")
-
-require("lazy").setup("plugins")
-
-vim.cmd.colorscheme("catppuccin")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
