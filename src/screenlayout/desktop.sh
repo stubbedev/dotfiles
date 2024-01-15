9bcebc5be6a941a3ce03f6f467728f2adbdd8c48
Alexander Stage <abs@stubbe.dev>
2024-01-15 20:30:48 +0100

    removed null video out

--- src/screenlayout/desktop.sh
+++ src/screenlayout/desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --rate 144.00 --pos 0x0 --rotate left --output DP-2 --off --output DP-3 --off --output HDMI-1 --primary --mode 2560x1440 --rate 144.00 --pos 1080x0 --rotate normal --output DP-4 --off --output DP-5 --off --output None-1-1 --off
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --rate 144.00 --pos 0x0 --rotate left --output DP-2 --off --output DP-3 --off --output HDMI-1 --primary --mode 2560x1440 --rate 144.00 --pos 1080x0 --rotate normal --output DP-4 --off --output DP-5 --off
