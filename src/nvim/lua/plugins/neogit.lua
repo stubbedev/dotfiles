10b35da5ce903e48b1ca97d926f9c0c0d6e44409
stubbe <abs@clerk.io>
2024-02-14 09:21:12 +0000

    changed neogit cmd

--- src/nvim/lua/plugins/neogit.lua
+++ src/nvim/lua/plugins/neogit.lua
@@ -10,7 +10,7 @@ return {
     },
     config = true,
     keys = {
-      { '<leader>g', '<cmd>lua require("neogit").open({ kind = "auto" })<cr>', desc = 'Neogit' }
-    }
-  }
+      { "<C-g>", '<cmd>lua require("neogit").open({ kind = "auto" })<cr>', desc = "Neogit" },
+    },
+  },
 }
