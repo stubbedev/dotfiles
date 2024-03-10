8bef5259f82db9a9934691bb68a2d08d57d42341
Alexander Stage <abs@stubbe.dev>
2024-03-10 13:48:05 +0000

    fix screen layout

--- src/screenlayout/desktop.sh
+++ src/screenlayout/desktop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output HDMI-1 --rate 144.0 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-2 --off --output HDMI-2 --rate 144.0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-3 --off
+xrandr --output DP-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --rate 144.0 --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate left --rate 144.0 --output DP-3 --off
+
 
