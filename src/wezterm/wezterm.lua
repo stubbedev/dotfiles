bbab7032d6641aa7c912b717d12a93b9765c1781
Alexander Stage <abs@stubbe.dev>
2024-08-17 13:12:25 +0200

    update wezterm shortcuts

--- src/wezterm/wezterm.lua
+++ src/wezterm/wezterm.lua
@@ -1,22 +1,25 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.use_resize_increments = true
 config.disable_default_key_bindings = true
 config.keys = {
-  {
-    key = 'P',
-    mods = 'CTRL',
-    action = wezterm.action.ActivateCommandPalette,
-  },
+	{
+		key = "P",
+		mods = "CTRL",
+		action = wezterm.action.ActivateCommandPalette,
+	},
+	{ key = "V", mods = "CTRL | SHIFT", action = act.PasteFrom("Clipboard") },
+	{ key = "V", mods = "CTRL | SHIFT", action = act.PasteFrom("PrimarySelection") },
 }
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
