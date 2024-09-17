e7617baf6b36dcdb0a297bb3399f2f42f26426b8
Alexander Stage <abs@stubbe.dev>
2024-09-17 13:17:07 +0200

    fix dict access

--- src/nvim/lua/plugins/php.lua
+++ src/nvim/lua/plugins/php.lua
@@ -43,7 +43,7 @@ return {
 
       require("nvim-treesitter.configs").setup(opts)
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
+      parser_config['blade'] = {
         install_info = {
           url = "https://github.com/EmranMR/tree-sitter-blade",
           files = { "src/parser.c" },
