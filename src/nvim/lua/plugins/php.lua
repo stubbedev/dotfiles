3bd137a6420dabf4455ac1d104cb4d3827639c13
Alexander Stage <abs@stubbe.dev>
2024-09-17 13:12:22 +0200

    remove phpcs linter

--- src/nvim/lua/plugins/php.lua
+++ src/nvim/lua/plugins/php.lua
@@ -9,6 +9,16 @@ return {
     },
   },
   {
+    -- Remove phpcs linter.
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+  {
     -- Add neotest-pest plugin for running PHP tests.
     -- A package is also available for PHPUnit if needed.
     "nvim-neotest/neotest",
