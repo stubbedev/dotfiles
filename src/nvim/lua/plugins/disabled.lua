7d73992bc67966c0094d8450b88a2ab3a0598b1a
Alexander Stage <abs@stubbe.dev>
2024-09-23 20:02:37 +0200

    reenble dashboard

--- src/nvim/lua/plugins/disabled.lua
+++ src/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
