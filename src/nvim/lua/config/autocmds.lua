00457ed64bacfbaf6398e6029fc56948c6025bba
Stubbe <abs@clerk.io>
2024-09-27 10:24:52 +0200

    remove func

--- src/nvim/lua/config/autocmds.lua
+++ src/nvim/lua/config/autocmds.lua
@@ -2,9 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 -- wrap and check for spell in text filetypes
-local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "coffee" },
