f4774c5cb50f845485108705b60133bd52ae942d
Alexander Stage <abs@stubbe.dev>
2024-10-18 23:08:26 +0200

    comment disabled core

--- src/nvim/lua/config/lazy.lua
+++ src/nvim/lua/config/lazy.lua
@@ -60,8 +60,8 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        "matchit",
-        "matchparen",
+        -- "matchit",
+        -- "matchparen",
         "netrwPlugin",
         "tarPlugin",
         "tohtml",
