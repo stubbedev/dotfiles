61e8e4bced190471df79a412e0795f45d20dca01
Alexander Stage <abs@stubbe.dev>
2024-10-18 21:00:19 +0200

    remove plugins

--- src/nvim/lua/plugins/disabled.lua
+++ src/nvim/lua/plugins/disabled.lua
@@ -1,8 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
-  -- { "nvimdev/dashboard-nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
--- src/nvim/lua/plugins/poweryank.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "haya14busa/vim-poweryank",
-  opts = {},
-  keys = {
-    { "<leader>y", "<Plug>(operator-poweryank-osc52)", desc = "Copy text to system clipboard" },
-  },
-}
