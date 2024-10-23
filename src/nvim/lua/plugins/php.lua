b9ada3aa05c5990eb7776f8f5116771caf95526f
Alexander Stage <abs@stubbe.dev>
2024-10-23 20:04:13 +0200

    added smartyank

--- src/nvim/lua/plugins/php.lua
+++ src/nvim/lua/plugins/php.lua
@@ -30,8 +30,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
+        "html",
         "blade",
         "php_only",
+        "php",
+        "bash"
       })
     end,
     config = function(_, opts)
