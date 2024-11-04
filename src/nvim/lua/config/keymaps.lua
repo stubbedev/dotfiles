c2a53963d41f71ac8d0868f292ef6067a4dfb531
Alexander Stage <abs@stubbe.dev>
2024-11-04 19:54:46 +0100

    changed lazyvim keybind

--- src/nvim/lua/config/keymaps.lua
+++ src/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>le", "<cmd>LazyExtras<cr>", { desc = "LazyExtras" })
