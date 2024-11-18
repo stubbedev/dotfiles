4d3ca1a34c4a1ce3d6ddab4f60372700812aa0ba
Alexander Stage <abs@stubbe.dev>
2024-11-18 13:43:42 +0100

    added coffeescript lsp

--- src/nvim/lua/plugins/coffee.lua
+++ src/nvim/lua/plugins/coffee.lua
@@ -4,4 +4,15 @@ return {
     event = "VeryLazy",
     config = function() end,
   },
+  {
+    "phil294/coffeesense",
+    config = function()
+      require("lspconfig").coffeesense.setup({
+        cmd = { "coffeesense-language-server", "--stdio" }, -- Command to start the language server
+        filetypes = { "coffee" }, -- Filetypes to attach the server to
+        root_dir = require("lspconfig").util.root_pattern(".git", "package.json"), -- Root directory for the project
+        settings = {}, -- Additional settings, if any
+      })
+    end,
+  },
 }
