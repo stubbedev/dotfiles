55ade6191d75322b16424bf6741428ef19abc28d
Alexander Stage <abs@stubbe.dev>
2024-11-19 09:56:34 +0100

    more coffeescript linter

--- src/nvim/lua/config/autocmds.lua
+++ src/nvim/lua/config/autocmds.lua
@@ -12,25 +12,41 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "FileWritePre", "BufWritePre" }, {
-  pattern = { "*.coffee" },
-  callback = function()
-    vim.cmd(":CoffeeLint | cwindow")
-  end,
-})
-
 vim.api.nvim_create_autocmd({ "FileWritePost", "BufWritePost" }, {
   pattern = { "*.coffee" },
   callback = function()
     local repo_name = vim.fn.system("basename `git rev-parse --show-toplevel 2>/dev/null` 2>/dev/null"):gsub("%s+", "")
     if repo_name == "clerk.js" then
-      vim.loop.spawn("bash", {
-        args = {
-          "-c",
-          "source venv/bin/activate && ./build.sh --debug && \\cp -rf clerk.js ../live.clerk.io/live.clerk.io",
-        },
-        detached = true,
-      })
+      vim.fn.jobstart(
+        "bash -c 'source venv/bin/activate && ./build.sh --debug && \\cp -rf clerk.js ../live.clerk.io/live.clerk.io'",
+        {
+          on_exit = function(_, code)
+            if code ~= 0 then
+              vim.notify("Build process exited with code " .. code, vim.log.levels.WARN)
+            end
+          end,
+        }
+      )
     end
+    vim.cmd(":CoffeeLint | cwindow")
+    -- vim.fn.jobstart("coffeelint " .. vim.api.nvim_buf_get_name(0), {
+    --   on_stdout = function(_, data)
+    --     vim.notify(data, vim.log.levels.INFO)
+    --     if data and #data > 0 then
+    --       vim.fn.setqflist({}, " ", { title = "CoffeeLint", lines = data })
+    --       vim.cmd("cwindow")
+    --     end
+    --   end,
+    -- on_stderr = function(_, data)
+    --   if data and #data > 0 then
+    --     vim.notify("CoffeeLint error: " .. table.concat(data, "\n"), vim.log.levels.ERROR)
+    --   end
+    -- end,
+    -- on_exit = function(_, code)
+    --   if code ~= 0 then
+    --     vim.notify("CoffeeLint exited with code " .. code, vim.log.levels.WARN)
+    --   end
+    -- end,
+    -- })
   end,
 })
