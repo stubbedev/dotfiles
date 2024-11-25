b5d0896f3e2a659d520a72d77026608fb460be45
Alexander Stage <abs@stubbe.dev>
2024-11-25 14:03:05 +0100

    added smear cursor gfx

---
+++ src/nvim/lua/plugins/smear.lua
@@ -0,0 +1,14 @@
+return {
+  -- Smooth cursor movement.
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  },
+  -- Smooth scrolling.
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({})
+    end,
+  },
+}
