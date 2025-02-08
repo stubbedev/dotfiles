27500a45a21b8d5d621f396f92a65ef6f0fa5482
Alexander Stage <abs@stubbe.dev>
2025-02-08 15:56:14 +0100

    actually disable monitor

--- src/hypr/scripts/lid-close-open.sh
+++ src/hypr/scripts/lid-close-open.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Find the built-in monitor dynamically (usually eDP or LVDS)
+builtin_monitor=$(hyprctl monitors | grep -i "eDP" | awk '{print $2}')
+if [ -z "$builtin_monitor" ]; then
+  builtin_monitor=$(hyprctl monitors | grep -i "LVDS" | awk '{print $2}')
+fi
+if [ -n "$builtin_monitor" ]; then
+  echo "$builtin_monitor" >/tmp/builtin_monitor
+fi
+if [ -z "$builtin_monitor" ] && [ -f /tmp/builtin_monitor ]; then
+  builtin_monitor="$(cat /tmp/builtin_monitor)"
+fi
+
 # Get the action (lid-close or lid-open) passed as an argument
 function get_action {
   if [ ! -f /tmp/lid_closed ]; then
@@ -12,33 +24,16 @@ function get_action {
 }
 action=$(get_action)
 
-# Find the built-in monitor dynamically (usually eDP or LVDS)
-builtin_monitor=$(hyprctl monitors | grep -i "eDP" | awk '{print $2}')
-if [ -z "$builtin_monitor" ]; then
-  builtin_monitor=$(hyprctl monitors | grep -i "LVDS" | awk '{print $2}')
-fi
-
 # Get the list of all active monitors
 monitors=$(hyprctl monitors | grep "Monitor " | awk '{print $2}')
 monitors=($monitors)
 
-wspaces=$(hyprctl workspaces | grep "$builtin_monitor" | awk '{print $3}')
-wspaces=($wspaces)
-wspace_indices=("${!wspaces[@]}")
-last_ws_index=${wspace_indices[-1]}
-last_ws=${wspaces[$last_ws_index]}
-
 if [ ${#monitors[@]} -gt 1 ]; then
-  next_monitor=${monitors[1]}
   if [ "$action" == "close" ]; then
-    for ws in "${wspaces[@]}"; do
-      hyprctl dispatch moveworkspacetomonitor "$ws" "$next_monitor"
-    done
-    hyprctl dispatch dpms off "$builtin_monitor"
+    hyprctl keyword monitor "$builtin_monitor", disable
   fi
 fi
 
 if [ "$action" == "open" ]; then
-  hyprctl dispatch dpms on "$builtin_monitor"
-  hyprctl dispatch moveworkspacetomonitor "${last_ws}" "$builtin_monitor"
+  hyprctl keyword monitor "$builtin_monitor", enable
 fi
