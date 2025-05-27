79e989b2178a6f20c4802a0a82158a5ac8becbb5
Alexander Bugge Stage <abs@stubbe.dev>
2025-05-27 22:04:05 +0200

    added more waybar stuff

---
+++ src/hypr/scripts/wait_for_power_profiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SERVICE="power-profiles-daemon.service"
+
+# Wait until the service is active
+while ! systemctl is-active --quiet "$SERVICE"; do
+    sleep 5
+done
+
+# Send the signal to Waybar once
+pkill -SIGUSR2 waybar
+
