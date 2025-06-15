d16954844252a07fc45a3f7de341e68583965874
Alexander Bugge Stage <abs@stubbe.dev>
2025-06-15 19:10:49 +0200

    vesktop

--- src/nix/home-manager/packages/app.nix
+++ src/nix/home-manager/packages/app.nix
@@ -4,4 +4,5 @@ with pkgs;[
   (config.lib.nixGL.wrap mongodb-compass)
   (config.lib.nixGL.wrap alacritty)
   (config.lib.nixGL.wrap mailspring)
+  mutt
 ]
--- src/nix/home-manager/programs/vesktop.nix
+++ src/nix/home-manager/programs/vesktop.nix
@@ -1,14 +1,12 @@
-{...}:
-{
+{ pkgs, config, ... }: {
   programs.vesktop = {
     enable = true;
+    package = (config.lib.nixGL.wrap pkgs.vesktop);
     vencord.settings = {
       autoUpdate = true;
       autoUpdateNotification = false;
       notifyAboutUpdates = false;
-      plugins = {
-        FakeNitro.enabled = true;
-      };
+      plugins = { FakeNitro.enabled = true; };
     };
   };
 }
