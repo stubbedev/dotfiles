59138859bc996a578fab29832ee7d5674ce31f64
Alexander Bugge Stage <abs@stubbe.dev>
2025-06-15 10:31:02 +0200

    added code companion keybinds

--- src/nvim/lua/plugins/codecompanion.lua
+++ src/nvim/lua/plugins/codecompanion.lua
@@ -10,11 +10,29 @@ return {
       opts = {
         log_level = "DEBUG",
       },
+      display = {
+        action_palette = {
+          width = 95,
+          height = 10,
+          prompt = "CodeCompanion Actions",
+          provider = "fzf_lua",
+          opts = {
+            show_default_actions = true, -- Show the default actions in the action palette?
+            show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+          },
+        },
+      },
     },
-    -- keys = {},
+    keys = {
+      { "<leader>aa", "<cmd>CodeCompanion Toggle<cr>", desc = "CodeCompanion Toggle Chat" },
+      { "<leader>ae", "<cmd>CodeCompanion Add<cr>", desc = "CodeCompanion Add Buffer" },
+      { "<leader>ap", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions" },
+      { "<leader>aq", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion Prompt" },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "ibhagwan/fzf-lua",
     },
   },
 }
