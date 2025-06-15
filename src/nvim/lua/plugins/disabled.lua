5973e2017c587e7bba471eacbf4a29612ff38928
Alexander Bugge Stage <abs@stubbe.dev>
2025-06-15 10:01:15 +0200

    disable snacks explorer

--- src/nvim/lua/plugins/disabled.lua
+++ src/nvim/lua/plugins/disabled.lua
@@ -4,6 +4,9 @@ return {
   { "goolord/alpha-nvim", enabled = false },
   -- { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "folke/snacks.nvim", opts = {
+    explorer = { enabled = false },
+  } },
   {
     "neovim/nvim-lspconfig",
     opts = {
