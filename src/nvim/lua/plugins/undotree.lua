0ba532bee54534880d7d777abd738a59cbc583ac
Alexander Bugge Stage <abs@stubbe.dev>
2025-07-05 20:38:51 +0200

    fix formatting

--- src/nvim/lua/plugins/undotree.lua
+++ src/nvim/lua/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   {
     'mbbill/undotree',
     event = "VeryLazy",
-    config = function ()
+    config = function()
       vim.keymap.set('n', '<leader>z', vim.cmd.UndotreeToggle, { desc = "Undotree toggle." })
       vim.keymap.set('v', '<leader>z', vim.cmd.UndotreeToggle, { desc = "Undotree toggle." })
     end
