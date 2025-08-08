3ad41a328fd1f53a26b3d8218ccc9da9d52d7c46
Alexander Bugge Stage <abs@stubbe.dev>
2025-08-08 07:30:11 +0200

    comment nix packages

--- src/nix/home-manager/packages/app.nix
+++ src/nix/home-manager/packages/app.nix
@@ -1,8 +1,8 @@
 { pkgs, config, ... }:
 with pkgs; [
-  (config.lib.nixGL.wrap ghostty)
   (config.lib.nixGL.wrap mongodb-compass)
   (config.lib.nixGL.wrap alacritty)
-  (config.lib.nixGL.wrap mailspring)
-  (config.lib.nixGL.wrap dbeaver-bin)
+  # (config.lib.nixGL.wrap ghostty)
+  # (config.lib.nixGL.wrap mailspring)
+  # (config.lib.nixGL.wrap dbeaver-bin)
 ]
