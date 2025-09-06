81f831b2d8adb47f79d41a5e6ef38c72bf680a15
Alexander Stage <abs@stubbe.dev>
2025-09-06 12:18:53 +0200

    update nix home manager

--- src/nix/home-manager/home.nix
+++ src/nix/home-manager/home.nix
@@ -33,7 +33,11 @@ in {
   imports = importedProgramsAndServices;
 
   home.file = {
-    ".zshrc".text = "source ${config.home.homeDirectory}/.stubbe/src/zsh/init";
+    ".zshrc".text = ''
+      if [[ -f "${config.home.homeDirectory}/.stubbe/src/zsh/init" ]]; then
+        source ${config.home.homeDirectory}/.stubbe/src/zsh/init
+      fi
+      ''
     ".ideavimrc".source = ./../../ideavim/ideavimrc;
     ".tmux.conf".source = ./../../tmux/tmux.conf;
     ".config/lazygit/config.yml".source = ./../../lazygit/config.yml;
