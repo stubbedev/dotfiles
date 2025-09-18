99cc885354972c24491d439aa726d9c7c75e6530
Alexander Stage <abs@stubbe.dev>
2025-09-18 19:32:25 +0200

    add ghostty

--- src/nix/home-manager/packages/applications.nix
+++ src/nix/home-manager/packages/applications.nix
@@ -2,10 +2,6 @@
 with pkgs; [
   (config.lib.nixGL.wrap mongodb-compass)
   (config.lib.nixGL.wrap alacritty)
-
-  # Commented out applications (uncomment as needed)
-  # (config.lib.nixGL.wrap ghostty)       # Alternative terminal
-  # (config.lib.nixGL.wrap mailspring)    # Email client
-  # (config.lib.nixGL.wrap dbeaver-bin)   # Database client
+  (config.lib.nixGL.wrap ghostty)
 ]
 
