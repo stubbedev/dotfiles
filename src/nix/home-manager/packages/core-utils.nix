d252fe3e2cdf11eb7a6e6978fba9d1d681520ed1
Alexander Stage <abs@stubbe.dev>
2025-10-18 20:28:50 +0200

    update core utils

---
+++ src/nix/home-manager/packages/core-utils.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+with pkgs; [
+  gcc
+  gnumake
+  git
+  curl
+  gnutar
+  coreutils
+  cmake
+  pkg-config
+  gettext
+  libtool
+  autoconf
+  automake
+  jq
+  gawk
+  gnugrep
+]
+
