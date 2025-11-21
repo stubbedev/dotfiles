f36765c91d93ebab9a7f635df34536dd6853b514
Alexander Bugge Stage <abs@stubbe.dev>
2025-11-21 21:39:25 +0100

    added more icons

--- src/nix/home-manager/packages/theming.nix
+++ src/nix/home-manager/packages/theming.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 with pkgs; [
   nerd-fonts.jetbrains-mono
+  font-awesome
   adwaita-fonts
   adwaita-icon-theme
   vimix-icon-theme
+  papirus-icon-theme
+  hicolor-icon-theme
   rose-pine-gtk-theme
   gtk4-layer-shell
 ]
