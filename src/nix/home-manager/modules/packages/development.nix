f4aae19a69c0de26ebf78740dd5584920c213834
Alexander Bugge Stage <abs@stubbe.dev>
2026-03-23 21:16:41 +0100

    added php

--- src/nix/home-manager/modules/packages/development.nix
+++ src/nix/home-manager/modules/packages/development.nix
@@ -14,6 +14,47 @@ _: {
       luajitBin = pkgs.writeShellScriptBin "luajit" ''
         exec ${pkgs.luajit}/bin/luajit "$@"
       '';
+      php = pkgs.php84.buildEnv {
+        extensions =
+          { all, ... }:
+          builtins.attrValues (removeAttrs all [
+            "blackfire"      # proprietary, requires license
+            "couchbase"      # broken
+            "datadog_trace"  # broken
+            "ioncube-loader" # proprietary loader
+            "oci8"           # requires Oracle client
+            "openssl-legacy" # removed from nixpkgs
+            "parallel"       # broken
+            "pdo_oci"        # requires Oracle client
+            "php-spx"        # deprecated alias for spx
+            "relay"          # proprietary
+            "tideways"       # unsupported PHP version
+            "swoole"         # conflicts with openswoole (duplicate function names)
+            "openswoole"     # conflicts with swoole; exclude both to avoid duplicate symbols
+            "xml"            # statically compiled into PHP base; loading as shared ext causes duplicate warning
+            "snuffleupagus"  # security hardening module that requires a config file to be useful
+          ]);
+        extraConfig = ''
+          memory_limit = 4G
+          post_max_size = 2G
+          upload_max_filesize = 2G
+          max_input_time = 300
+          max_execution_time = 300
+        '';
+      };
+      phpFpmBin = pkgs.writeShellScriptBin "php-fpm" ''
+        cfg="''${XDG_CONFIG_HOME:-$HOME/.config}/php/php-fpm.conf"
+        case " $* " in
+          *" -y "*|*" --fpm-config "*) exec ${php}/bin/php-fpm "$@" ;;
+          *) exec ${php}/bin/php-fpm -y "$cfg" "$@" ;;
+        esac
+      '';
+      # Expose all php binaries except php-fpm (replaced by phpFpmBin wrapper above)
+      phpBins = pkgs.symlinkJoin {
+        name = "php-with-extensions-8.4.19";
+        paths = [ php ];
+        postBuild = "rm $out/bin/php-fpm";
+      };
     in
     lib.mkIf config.features.development {
       home.packages = with pkgs; [
@@ -42,11 +83,9 @@ _: {
         mongodb-tools
         mongosh
 
-        # PHP 8.4 with all extensions
-        (php84.withExtensions (
-          { all, ... }:
-          builtins.attrValues all
-        ))
+        # PHP 8.4 with all evaluable extensions and generous runtime limits
+        phpBins
+        phpFpmBin
 
         # PHP tools (CLI)
         mago
@@ -59,5 +98,7 @@ _: {
         networkmanager-openconnect
         openconnect
       ];
+
+      xdg.configFile."php/php-fpm.conf".source = ../../../../php/php-fpm.conf;
     };
 }
