ab422e06a7adb298b68773c2e81af8ce9e5c0cab
Alexander Bugge Stage <abs@stubbe.dev>
2026-04-11 19:46:00 +0200

    update scrolling behavior

--- src/hypr/scripts/wrapper.scrollworkspace.sh
+++ src/hypr/scripts/wrapper.scrollworkspace.sh
@@ -3,7 +3,7 @@
 DIRECTION=$1
 
 if [ "$DIRECTION" = "next" ]; then
-    hyprctl dispatch workspace e+1
+  hyprctl dispatch workspace e+1
 elif [ "$DIRECTION" = "prev" ]; then
-    hyprctl dispatch workspace e-1
+  hyprctl dispatch workspace e-1
 fi
