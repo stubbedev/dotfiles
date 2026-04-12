e1f169fb35cc88293cae40dceaacf8fb5822ff13
Alexander Bugge Stage <abs@stubbe.dev>
2026-04-12 21:11:06 +0200

    update intel pstate

--- src/nix/home-manager/modules/activation/_privileged/setup-grub-intel-pstate.nix
+++ src/nix/home-manager/modules/activation/_privileged/setup-grub-intel-pstate.nix
@@ -14,26 +14,20 @@ _: {
       allowing software governors (schedutil, performance, etc.) to
       control CPU frequency scaling instead of hardware (HWP).
 
-      After installation, you will need to:
-        1. Run: sudo update-grub
-        2. Reboot your system
+      Will take effect after reboot.
     '';
     promptQuestion = "Install GRUB config for intel_pstate passive mode?";
     actionScript = ''
       tmpfile=$(mktemp)
       trap 'rm -f "$tmpfile"' EXIT
       cat > "$tmpfile" << 'EOF'
-      # managed-by: home-manager grub-intel-pstate v1
       # Force intel_pstate to use passive mode for software-controlled CPU frequency scaling
       GRUB_CMDLINE_LINUX_DEFAULT="''${GRUB_CMDLINE_LINUX_DEFAULT} intel_pstate=passive"
       EOF
       sudo install -d -m 0755 /etc/default/grub.d
       sudo install -m 0644 "$tmpfile" /etc/default/grub.d/intel-pstate-passive.cfg
       sudo chown root:root /etc/default/grub.d/intel-pstate-passive.cfg
-      echo ""
-      echo "GRUB config installed. Run the following commands:"
-      echo "  sudo update-grub"
-      echo "  sudo reboot"
+      sudo update-grub
     '';
     skipMessage = "Skipped. You can install it later by running: home-manager switch --flake . --impure";
   };
