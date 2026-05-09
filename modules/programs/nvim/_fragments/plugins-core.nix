3febb11816625b7f4b5424aef13154f452b4a1a4
Alexander Bugge Stage <abs@stubbe.dev>
2026-05-09 12:10:38 +0200

    update nvim dash

--- modules/programs/nvim/_fragments/plugins-core.nix
+++ modules/programs/nvim/_fragments/plugins-core.nix
@@ -31,7 +31,6 @@ in
               { icon = " "; key = "n"; desc = "New File"; action = ":ene | startinsert"; }
               { icon = " "; key = "g"; desc = "Find Text"; action = ":lua Snacks.dashboard.pick('live_grep')"; }
               { icon = " "; key = "r"; desc = "Recent Files"; action = ":lua Snacks.dashboard.pick('oldfiles')"; }
-              { icon = " "; key = "c"; desc = "Config"; action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})"; }
               { icon = " "; key = "s"; desc = "Restore Session"; action = ":lua require('persistence').load()"; }
               { icon = " "; key = "q"; desc = "Quit"; action = ":qa"; }
             ];
@@ -41,22 +40,6 @@ in
             {
               section = "keys";
               gap = 1;
-              padding = 1;
-            }
-            {
-              pane = 2;
-              icon = " ";
-              title = "Recent Files";
-              section = "recent_files";
-              indent = 2;
-              padding = 1;
-            }
-            {
-              pane = 2;
-              icon = " ";
-              title = "Projects";
-              section = "projects";
-              indent = 2;
               padding = 1;
             }
           ];
