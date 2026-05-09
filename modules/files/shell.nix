8b3391b7ca46d7e9cccde87e4fc9d50e21ac6efc
Alexander Bugge Stage <abs@stubbe.dev>
2026-05-09 20:00:06 +0200

    skip global compinit

--- modules/files/shell.nix
+++ modules/files/shell.nix
@@ -9,6 +9,14 @@ _: {
     }:
     lib.mkIf config.features.desktop {
       home.file = {
+        # Ubuntu's /etc/zsh/zshrc runs compinit with the system-default
+        # fpath, overwriting ~/.zcompdump before our init can extend
+        # fpath with ~/.nix-profile/share/zsh/site-functions. Setting
+        # this in .zshenv (sourced before /etc/zshrc) suppresses that
+        # global compinit so our init.zsh produces the only dump.
+        ".zshenv".text = ''
+          skip_global_compinit=1
+        '';
         ".zshrc".text = ''
           if [[ -f "${constants.paths.zsh}/init" ]]; then
             source ${constants.paths.zsh}/init
