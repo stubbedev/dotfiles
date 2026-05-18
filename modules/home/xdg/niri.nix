f1508bdc4dbf9032ac119e09bf0b52ad27375e4c
Alexander Bugge Stage <abs@stubbe.dev>
2026-05-18 20:04:48 +0200

    update niri cursors as well

--- modules/home/xdg/niri.nix
+++ modules/home/xdg/niri.nix
@@ -1,7 +1,9 @@
-_: {
+{ self, ... }:
+{
   flake.modules.homeManager.xdgNiri =
     {
       config,
+      constants,
       lib,
       homeLib,
       ...
@@ -9,9 +11,24 @@ _: {
     lib.mkIf config.features.niri {
       xdg.configFile =
         homeLib.xdgSources [
-          "niri/config.kdl"
           "niri/hypridle.conf"
           "niri/scripts"
-        ];
+        ]
+        // {
+          # Render niri's config.kdl through Nix so cursor theme/size flow
+          # from constants.theme. Source under src/niri/config.kdl uses
+          # @XCURSOR_THEME@ / @XCURSOR_SIZE@ markers; substituteFile reads
+          # the file and replaces them at HM build time.
+          "niri/config.kdl" = {
+            text = homeLib.substituteFile {
+              file = self + "/src/niri/config.kdl";
+              vars = {
+                XCURSOR_THEME = constants.theme.cursor;
+                XCURSOR_SIZE = toString constants.theme.cursorSize;
+              };
+            };
+            force = true;
+          };
+        };
     };
 }
