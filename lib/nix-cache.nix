0a3ff407743633a043eb37d356d632d3c36e6d7c
Alexander Bugge Stage <abs@stubbe.dev>
2026-06-17 21:39:22 +0200

    nix: add treeman + srv attic substituters
    Pull prebuilt treeman/srv nix builds from the self-hosted cache
    (nix.stubbe.dev) instead of recompiling locally.

--- lib/nix-cache.nix
+++ lib/nix-cache.nix
@@ -1,12 +1,16 @@
 {
   substituters = [
     "https://nix.stubbe.dev/wayle"
+    "https://nix.stubbe.dev/treeman"
+    "https://nix.stubbe.dev/srv"
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
     "https://wayle.cachix.org"
   ];
   trusted-public-keys = [
     "wayle:XD2O2h1Mmka+VegRi2JY7ywNbG9al+TUAZp6CObizFU="
+    "treeman:wK3AZux2l7fX+L4Lo9OLh7zzQAC/OJDUQanldOgNhO4="
+    "srv:zIqJz/1IhxGYhVu5uihWmWHyTSv6GPzCAk/6NCgrAMo="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "wayle.cachix.org-1:9sDbCI21ymzOlqp5NWgXq0tw+RE4p35WbSlt96xBNH4="
