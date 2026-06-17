493ab49d26cf7734afcdcd425fe3e0cbce23db4e
Alexander Bugge Stage <abs@stubbe.dev>
2026-06-17 07:26:33 +0200

    update nixpkgs

--- lib/nix-cache.nix
+++ lib/nix-cache.nix
@@ -2,9 +2,11 @@
   substituters = [
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
+    "https://wayle.cachix.org"
   ];
   trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "wayle.cachix.org-1:9sDbCI21ymzOlqp5NWgXq0tw+RE4p35WbSlt96xBNH4="
   ];
 }
