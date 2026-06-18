88cacbf452cf6754d9a3556fddd8c09f8a7c8885
Alexander Bugge Stage <abs@stubbe.dev>
2026-06-18 07:27:19 +0200

    nix: drop wayle.cachix.org substituter
    Use own attic (nix.stubbe.dev/wayle) only. cachix mirror redundant
    once attic push wired up.

--- lib/nix-cache.nix
+++ lib/nix-cache.nix
@@ -5,7 +5,6 @@
     "https://nix.stubbe.dev/srv"
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
-    "https://wayle.cachix.org"
   ];
   trusted-public-keys = [
     "wayle:XD2O2h1Mmka+VegRi2JY7ywNbG9al+TUAZp6CObizFU="
@@ -13,6 +12,5 @@
     "srv:zIqJz/1IhxGYhVu5uihWmWHyTSv6GPzCAk/6NCgrAMo="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "wayle.cachix.org-1:9sDbCI21ymzOlqp5NWgXq0tw+RE4p35WbSlt96xBNH4="
   ];
 }
