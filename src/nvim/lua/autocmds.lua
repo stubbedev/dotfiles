bb0fdcb69b2c2b23c7b3fb8ae7dd51e0169a050d
Alexander Bugge Stage <abs@stubbe.dev>
2026-08-30 21:16:11 +0200

    fix autocms

--- src/nvim/lua/autocmds.lua
+++ src/nvim/lua/autocmds.lua
@@ -94,7 +94,9 @@ vim.api.nvim_create_autocmd("VimEnter", {
   group = augroup("oil_start"),
   callback = function()
     if vim.fn.argc() == 0 and vim.api.nvim_buf_get_name(0) == "" and vim.fn.line2byte(vim.fn.line("$")) == -1 then
-      require("oil").open()
+      vim.schedule(function()
+        require("oil").open()
+      end)
     end
   end,
 })
